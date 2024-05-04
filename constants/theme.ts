ba39d7bea0b75334c7355755540c79ad570d7e33
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-05-04 18:59:09 -0300

    feat: add constants and utils

---
+++ constants/theme.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    white: "#fff",
+    black: "#000",
+    grayBackground: "#e5e5e5",
+    neutral: (opacity: number) => `rgba(10,10,10, ${opacity})`,
+};
+
+export const fontWeights = {
+    medium: "500",
+    semiBold: "600",
+    bold: "700",
+};
+
+export const radius = {
+    xs: 10,
+    sm: 12,
+    md: 14,
+    lg: 16,
+    xl: 18,
+};
