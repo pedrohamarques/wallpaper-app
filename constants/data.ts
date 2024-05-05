ef63ae1df13e36aaabfb348a387e8fc2a0ff2457
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-05-05 14:04:43 -0300

    feat: add categories list

---
+++ constants/data.ts
@@ -0,0 +1,24 @@
+export const categories = [
+    "backgrounds",
+    "fashion",
+    "nature",
+    "science",
+    "education",
+    "feelings",
+    "health",
+    "people",
+    "religion",
+    "places",
+    "animals",
+    "industry",
+    "computer",
+    "food",
+    "sports",
+    "transportation",
+    "travel",
+    "buildings",
+    "business",
+    "music",
+] as const;
+
+export type CategoriesTypes = (typeof categories)[number];
