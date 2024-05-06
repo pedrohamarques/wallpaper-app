821b51eb87cb89a0ada3890739c54f8929e74409
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-05-06 08:29:42 -0300

    feat: add packages and config babel to dotenv

--- babel.config.js
+++ babel.config.js
@@ -12,6 +12,19 @@ module.exports = function (api) {
                 },
             ],
             "react-native-reanimated/plugin",
+            [
+                "module:react-native-dotenv",
+                {
+                    envName: "APP_ENV",
+                    moduleName: "@env",
+                    path: ".env",
+                    blocklist: null,
+                    allowlist: null,
+                    safe: false,
+                    allowUndefined: true,
+                    verbose: false,
+                },
+            ],
         ],
     };
 };
---
+++ env.d.ts
@@ -0,0 +1,3 @@
+declare module "@env" {
+    export const PIXABAY_KEY: string;
+}
